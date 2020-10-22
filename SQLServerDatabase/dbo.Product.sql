326ec821d63f806e68b4984e6314021754341a9f
apaniagua3140 <alberto.paniagua3140@yahoo.com.mx>
2020-10-22 16:42:41 -0500

    Adding new column

---
+++ SQLServerDatabase/dbo.Product.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Product] (
+    [Id]   INT           NOT NULL,
+    [Name] NVARCHAR (50) NULL,
+    [Cost] INT           NULL,
+    [Color] NVARCHAR(50) NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
